714f12bc225d3316a7728dd16b4017766207f7ba
Spencer Dixon <spencercdixon@gmail.com>
2020-05-03 09:32:32 -0400

    Renderer: Adds a basic texture that we can draw into and sets pixels to white.

--- Emulator/main.cpp
+++ Emulator/main.cpp
@@ -1,23 +1,37 @@
 #include <SDL.h>
+#include <SD/Types.h>
 #include <iostream>
 
+const u16 WIN_HEIGHT = 640;
+const u16 WIN_WIDTH = 480;
+
 int main() {
-
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "Failed to initialize the SDL2 library\n";
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to init SDL");
         return -1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          680, 480,
-                                          0);
+    SDL_Window *window;
+    SDL_Renderer *renderer;
 
-    if (!window) {
-        std::cout << "Failed to create window\n";
-        return -1;
+    if (SDL_CreateWindowAndRenderer(WIN_HEIGHT, WIN_WIDTH, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
     }
+
+    // Window Config
+    SDL_SetWindowTitle(window, "Gameboy Color");
+    SDL_RenderSetLogicalSize(renderer, WIN_HEIGHT, WIN_WIDTH);
+
+    // Fill our buffer
+    auto texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WIN_HEIGHT,
+                                     WIN_WIDTH);
+    u32 myPixels[WIN_WIDTH * WIN_HEIGHT];
+    for (int i = 0; i < WIN_HEIGHT * WIN_WIDTH; i++) {
+        myPixels[i] = 0xffffffff;
+    }
+
+    SDL_UpdateTexture(texture, NULL, myPixels, WIN_WIDTH * sizeof(u32));
 
     SDL_Event e;
     bool quit = false;
@@ -27,6 +41,10 @@ int main() {
                 quit = true;
             }
         }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
 
     return 0;
