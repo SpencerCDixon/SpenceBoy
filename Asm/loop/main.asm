6fc24dbc9655f8279e1d2f28878828543559acfd
Spencer Dixon <spencercdixon@gmail.com>
2020-05-05 21:25:30 -0400

    Asm: Updates our loop example with some comments. I believe the asm is working as intended based on the registers command inside the SameBoy debugger

--- Asm/loop/main.asm
+++ Asm/loop/main.asm
@@ -1,9 +1,12 @@
 INCLUDE "hardware.inc"
 
+; The gameboy will load our code at address $100
 SECTION "Header", ROM0[$100]
 
+
+; There is a bunch of header junk starting at $104 so we need to immediately jump to our code
 EntryPoint:
-	jp Start ; 
+	jp Start; 
 
 REPT $150 - $104
 	db 0
@@ -12,7 +15,10 @@ ENDR
 SECTION "Game code", ROM0
 
 Start:
-	ld a, $03
+	ld a, 3 ; load a register with 3
+
+loop:
+	; do some work in our loop
 	dec a
-	jr nz, Start 
-	halt
+	jp nz, loop
+	halt ; checking registers shows a as 0
