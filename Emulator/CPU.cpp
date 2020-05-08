72ef92a2df90996ab6a967a58800e1aaf3dadbff
Spencer Dixon <spencercdixon@gmail.com>
2020-05-07 20:06:31 -0400

    Emulator: Adds register and more robust instruction abstraction

--- Emulator/CPU.cpp
+++ Emulator/CPU.cpp
@@ -23,6 +23,10 @@ loop:
 
  */
 
+void print_registers(const Registers& reg) {
+    dbg() << "a: " << reg.a << " b: " << reg.b << "\n REST TODO :-)";
+}
+
 void CPU::parse_rom()
 {
     dbg() << "CPU::parse_rom() parsing op codes from ROM path: " << m_rom_path;
@@ -38,15 +42,23 @@ void CPU::parse_rom()
     fread(buff, 10, 1, fp);
 
     for (int i = 0; i < 10; ++i) {
-        if (buff[i] == 0x3e) {
-            dbg() << "LD OpCode";
-            add_instruction(OpCode::LoadAD8);
-        } else if (buff[i] == 0x76) {
+        switch (buff[i]) {
+        case '\x3e':
+            add_instruction(OpCode::Load_A_D8);
+            break;
+        case '\x76':
             add_instruction(OpCode::Halt);
+            break;
+        case '\x3d':
+            add_instruction(OpCode::Dec_A);
+            break;
+        case '\xc2': // FIXME: why does this need to be '\xc2' instead of 0xc2. Should I be using \x syntax?
+            add_instruction(OpCode::Jump_NZ);
+            break;
         }
         printf("%x ", buff[i]);
     }
-    printf("finished parsing first 10 bytes");
+    printf("\nfinished parsing first 10 bytes\n");
 }
 
 void CPU::add_instruction(const OpCode& opcode)
--- Emulator/CPU.h
+++ Emulator/CPU.h
@@ -8,13 +8,35 @@
 #include <SD/Types.h>
 
 enum class OpCode {
-    LoadAD8,
+    Load_A_D8,
+    Dec_A,
+    Jump_NZ,
     Halt,
 };
 
+// FIXME: I really want some sort of Option<u16> for the operands
 struct Instruction {
     OpCode op_code;
+    u16 left_operand;
+    u16 right_operand;
+    // FIXME: wouldn't be necessary with Option<>
+    u8 operand_count; // 0 - just op code, 1 - left operand, 2 - right operand
 };
+
+struct Registers {
+    u8 a { 0 };
+    u8 f { 0 };
+    u8 b { 0 };
+    u8 c { 0 };
+    u8 d { 0 };
+    u8 e { 0 };
+    u8 h { 0 };
+    u8 l { 0 };
+    u16 stack_ptr { 0 };
+    u16 program_counter { 0 };
+};
+
+void print_registers(const Registers& reg);
 
 class CPU {
 
@@ -42,11 +64,17 @@ public:
     void print_op_code(const OpCode& code)
     {
         switch (code) {
-        case OpCode::LoadAD8:
-            dbg() << "LoadAD8";
+        case OpCode::Load_A_D8:
+            dbg() << "Load_A_D8";
             break;
         case OpCode::Halt:
             dbg() << "Halt";
+            break;
+        case OpCode::Dec_A:
+            dbg() << "Dec_A";
+            break;
+        case OpCode::Jump_NZ:
+            dbg() << "Jump_NZ";
             break;
         }
     }
@@ -54,8 +82,13 @@ public:
 private:
     void add_instruction(const OpCode& opcode);
 
+    // TODO:
+//    void add_instruction(const Instruction& instruction);
+
 private:
     const char* m_rom_path;
     Instruction* m_instructions;
-    u16 m_instruction_count { 0 };
+    u16 m_instruction_count { 0 }; // FIXME: Is this needed?
+
+    // TODO: Add registers!
 };
