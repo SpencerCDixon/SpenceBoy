79e6023a3ecea32c762df43023fd1eb5fc80f3ec
Spencer Dixon <spencercdixon@gmail.com>
2020-05-10 09:44:47 -0400

    Assert: Adds a useful 'error()' utility taken from K&R for quickly exiting on unrecoverable error

--- SD/Assertions.h
+++ SD/Assertions.h
@@ -1,6 +1,19 @@
 #pragma once
 
 #include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 #define ASSERT assert
 #define ASSERT_NOT_REACHED() assert(false)
+
+inline void error(const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    fprintf(stderr, "error: ");
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, "\n");
+    va_end(args);
+    exit(1);
+}
