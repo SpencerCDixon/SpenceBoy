a882189da8a89feff1f865ba705f66745a5a2945
Spencer Dixon <spencercdixon@gmail.com>
2020-06-22 19:54:34 -0400

    ASM: Update all the regularly worked on ROMs to put their output into the new Asm/ROMs destination

--- Asm/hello-world/build.sh
+++ Asm/hello-world/build.sh
@@ -5,5 +5,5 @@ rgbasm -o main.o main.asm
 rgblink -o hello-world.gb main.o
 rgbfix -v -p 0 hello-world.gb
 
-cp ./hello-world.gb ../../Tests/data
+cp ./hello-world.gb ../ROMs
 hexdump -C hello-world.gb
