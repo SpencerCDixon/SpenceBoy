c590f2a63406a136be0a3f8e7f6afe43fefecbfd
Spencer Dixon <spencercdixon@gmail.com>
2020-06-28 10:03:04 -0400

    SD: Improve Vector with more functionality

--- SD/Vector.h
+++ SD/Vector.h
@@ -8,39 +8,153 @@
 #include <SD/Types.h>
 #include <SD/Utility.h>
 
+#include <initializer_list>
 #include <new>
+
+template<typename VectorType, typename ElementType>
+class VectorIterator {
+public:
+    bool operator!=(const VectorIterator& other) const { return m_index != other.m_index; }
+    bool operator==(const VectorIterator& other) const { return m_index == other.m_index; }
+    bool operator<(const VectorIterator& other) const { return m_index < other.m_index; }
+    bool operator>(const VectorIterator& other) const { return m_index > other.m_index; }
+    bool operator>=(const VectorIterator& other) const { return m_index >= other.m_index; }
+    VectorIterator& operator++()
+    {
+        ++m_index;
+        return *this;
+    }
+    VectorIterator& operator--()
+    {
+        --m_index;
+        return *this;
+    }
+    VectorIterator operator-(size_t value) { return { m_vector, m_index - value }; }
+    VectorIterator operator+(size_t value) { return { m_vector, m_index + value }; }
+    VectorIterator& operator=(const VectorIterator& other)
+    {
+        m_index = other.m_index;
+        return *this;
+    }
+    ElementType& operator*() { return m_vector[m_index]; }
+    size_t operator-(const VectorIterator& other) { return m_index - other.m_index; }
+
+    bool is_end() const { return m_index == m_vector.size(); }
+    size_t index() const { return m_index; }
+
+private:
+    friend VectorType;
+    VectorIterator(VectorType& vector, size_t index)
+        : m_vector(vector)
+        , m_index(index)
+    {
+    }
+    VectorType& m_vector;
+    size_t m_index { 0 };
+};
 
 template<typename T>
 class Vector {
 public:
+    //
+    // Constructors
+    //
     Vector() { }
-    ~Vector()
+    Vector(std::initializer_list<T> list)
     {
-        clear();
+        ensure_capacity(list.size());
+        for (auto& item : list)
+            unchecked_append(item);
+    }
+    ~Vector() { clear(); }
+    Vector(const Vector& other)
+    {
+        ensure_capacity(other.size());
+        for (size_t i = 0; i < other.size(); ++i)
+            new (slot(i)) T(other.at(i));
+        m_size = other.size();
+    }
+    Vector(Vector&& other)
+        : m_size(other.m_size)
+        , m_capacity(other.m_capacity)
+        , m_data(other.m_data)
+    {
+        other.m_data = nullptr;
+        other.m_size = 0;
+        other.m_capacity = 0;
     }
 
-    T& at(size_t index)
+    //
+    // Getters
+    //
+    size_t capacity() const { return m_capacity; }
+    size_t size() const { return m_size; }
+    T* data() { return m_data; }
+    const T* data() const { return m_data; }
+
+    //
+    // Access
+    //
+    inline T& at(size_t index)
     {
-        ASSERT(index < m_size);
+        ASSERT(index < size());
         return data()[index];
     }
+    inline const T& at(size_t i) const
+    {
+        ASSERT(i < size());
+        return data()[i];
+    }
+    inline const T& operator[](size_t i) const { return at(i); }
+    inline T& operator[](size_t i) { return at(i); }
 
-    size_t capacity() const { return m_capacity; }
-    size_t size() { return m_size; }
+    const T& first() const { return at(0); }
+    T& first() { return at(0); }
+
+    const T& last() const { return at(size() - 1); }
+    T& last() { return at(size() - 1); }
+
     bool is_empty() const { return size() == 0; }
+
+    bool contains_slow(const T& value) const
+    {
+        for (size_t i = 0; i < size(); ++i) {
+            if (at(i) == value)
+                return true;
+        }
+        return false;
+    }
 
     //
     // Insertion/Removal
     //
-
-    template<class... Args>
-    void push(Args&&... args)
+    void unchecked_append(T&& value)
     {
-        grow_capacity(m_size + 1);
-        new (slot(m_size)) T(forward<Args>(args)...);
+        ASSERT((size() + 1) <= capacity());
+        new (slot(m_size)) T(move(value));
         ++m_size;
     }
 
+    void unchecked_append(const T& value)
+    {
+        unchecked_append(T(value));
+    }
+
+    void append(T&& value)
+    {
+        grow_capacity(size() + 1);
+        new (slot(m_size)) T(move(value));
+        ++m_size;
+    }
+
+    void append(const T& value)
+    {
+        append(T(value));
+    }
+
+    //
+    // Storage
+    //
     void grow_capacity(size_t needed_capacity)
     {
         if (m_capacity >= needed_capacity)
@@ -70,6 +184,13 @@ public:
         m_capacity = new_capacity;
     }
 
+    //
+    // Iteration
+    //
+    using Iterator = VectorIterator<Vector, T>;
+    Iterator begin() { return Iterator(*this, 0); }
+    Iterator end() { return Iterator(*this, size()); }
+
 private:
     void clear()
     {
@@ -79,13 +200,11 @@ private:
         }
     }
 
-    T* data() { return m_data; }
-
     T* slot(size_t i) { return &data()[i]; }
     const T* slot(size_t i) const { return &data()[i]; }
 
 private:
-    size_t m_capacity { 0 };
     size_t m_size { 0 };
+    size_t m_capacity { 0 };
     T* m_data { nullptr };
 };
--- Tests/TestVector.cpp
+++ Tests/TestVector.cpp
@@ -6,9 +6,6 @@
 #include <SD/LogStream.h>
 #include <SD/Vector.h>
 
-#include <stdio.h>
-#include <string.h>
-
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
     if (false)
@@ -19,9 +16,58 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
     Vector<u8> bytes;
 
     u8 second_value = 60;
-    bytes.push(42);
-    bytes.push(second_value);
+    bytes.append(42);
+    bytes.append(second_value);
 
-    ASSERT(bytes.at(0) == 42);
-    ASSERT(bytes.at(1) == 60);
+    ASSERT(bytes[0] == 42);
+    ASSERT(bytes[1] == 60);
+
+    // Empty
+    {
+        Vector<bool> v;
+        ASSERT(v.is_empty());
+    }
+
+    // First/Last
+    {
+        Vector<int> v;
+        v.append(1);
+        v.append(2);
+        v.append(3);
+        ASSERT(v.first() == 1);
+        ASSERT(v.last() == 3);
+    }
+
+    // Contains
+    {
+        Vector<int> v { 1, 2, 42, 4, 5 };
+        ASSERT(v.contains_slow(42));
+    }
+
+    // Copy Constructor
+    {
+        Vector<int> v1 { 1, 2, 3 };
+        auto v2 = v1;
+        ASSERT(v1.first() == 1);
+        ASSERT(v2.first() == 1);
+    }
+
+    // Move Constructor
+    {
+        Vector<int> v1 { 1, 2, 3 };
+        auto v2 = move(v1);
+        ASSERT(v1.is_empty());
+        ASSERT(v2.first() == 1);
+    }
+
+    // Iteration
+    {
+        int sum = 0;
+        Vector<int> v1 { 1, 2, 3 };
+
+        for (auto& val : v1)
+            sum += val;
+
+        ASSERT(sum == 6);
+    }
 }
